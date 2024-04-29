1552f46e9249a8a755ae97a0b7ee7f60d86e7ce2
James Nicholls <114191684+JNicholls2026@users.noreply.github.com>
2024-04-28 22:12:21 -0400

    Add files via upload

---
+++ portscanner3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+network=$1
+port=$2
+
+echo "host.port"
+for ((ip=1; ip<=254; ip++)); do
+	ipaddr="$1.$ip"
+	timeout .1 bash -c "echo >/dev/tcp/$ipaddr/$port" 2>/dev/null && echo "$ipaddr,$port"
+done
